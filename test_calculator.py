9328285e515b993aedadb308afd0c3202d008798
Daniel Brito <danielbrito@hotmail.es>
2021-03-13 16:52:24 +0100

    Test works

--- test_calculator.py
+++ test_calculator.py
@@ -5,10 +5,13 @@ import unittest
 import calculator
 
 
-class TestCalculator:
+class TestCalculator(unittest.TestCase):
 
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+
+if __name__ == '__main__':
+    unittest.main()
